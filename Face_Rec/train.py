509403760ea7c71c3a5fd3d32b15b329b23a4889
ReidVogel8 <166967314+ReidVogel8@users.noreply.github.com>
2025-03-26 09:39:24 -0600

    Update train.py

--- Face_Rec/train.py
+++ Face_Rec/train.py
@@ -5,7 +5,7 @@ import numpy as np
 import pickle
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-image_dir = os.path.join(BASE_DIR, "images")
+image_dir = os.path.join(BASE_DIR, "Images/Hunter")
 
 face_cascade = cv.CascadeClassifier(BASE_DIR + '/data/haarcascade_frontalface_default.xml')
 recognizer = cv.face.LBPHFaceRecognizer_create()
