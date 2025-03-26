872ba60972ee7ac51e7e1933a2bcb9235482e453
Jackson Kueny <jacksonkueny4@gmail.com>
2025-03-26 09:48:37 -0600

    Update newTrainer.py

--- Face_Rec/newTrainer.py
+++ Face_Rec/newTrainer.py
@@ -6,7 +6,7 @@ from PIL import Image
 
 # Paths
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-IMAGE_DIR = os.path.join(BASE_DIR, "images")
+IMAGE_DIR = os.path.join(BASE_DIR, "Images")
 CASCADE_PATH = cv.data.haarcascades + "haarcascade_frontalface_default.xml"
 
 # Load Haar Cascade for face detection
@@ -24,7 +24,7 @@ y_labels = []
 # Loop through images folder
 for root, dirs, files in os.walk(IMAGE_DIR):
     for file in files:
-        if file.endswith(("png", "jpg", "jpeg")):
+        if file.endswith(("png", "jpg", "jpeg", "HEIC")):
             path = os.path.join(root, file)
             label = os.path.basename(root).replace(" ", "_").lower()
 
