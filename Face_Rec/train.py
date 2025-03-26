b35851bd58ac29d776265c593276031da7fc6b1c
ReidVogel8 <166967314+ReidVogel8@users.noreply.github.com>
2025-03-26 10:38:22 -0600

    Update train.py

--- Face_Rec/train.py
+++ Face_Rec/train.py
@@ -4,11 +4,9 @@ import os
 import numpy as np
 import pickle
 
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-image_dir = os.path.join(BASE_DIR, "Images")
-
-# Load Haar cascade correctly
-cascade_path = os.path.join(BASE_DIR, 'data', 'haarcascade_frontalface_default.xml')
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))  # Get script directory
+IMAGE_DIR = os.path.join(BASE_DIR, "Images")  # Path to images
+CASCADE_PATH = cv.data.haarcascades + "haarcascade_frontalface_default.xml"
 face_cascade = cv.CascadeClassifier(cascade_path)
 
 # Check if cascade loaded
