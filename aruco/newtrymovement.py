6dc7b61418171cea96e293e84c268dec8e84efa9
Jackson Kueny <jacksonkueny4@gmail.com>
2025-04-09 12:57:23 -0600

    Update newtrymovement.py

--- aruco/newtrymovement.py
+++ aruco/newtrymovement.py
@@ -145,11 +145,10 @@ try:
                 cx = corners[i][0][:, 0].mean()
 
                 # Pan to keep centered
-                while frame_center_x - 75 < cx < frame_center_x + 75:
-                    if cx < frame_center_x - 75:
-                        robot.pan_right()
-                    if cx > frame_center_x + 75:
-                        robot.pan_left()
+                if cx < frame_center_x - 75:
+                    robot.pan_right()
+                if cx > frame_center_x + 75:
+                    robot.pan_left()
 
                 # Navigation logic based on camera position
                 if camera_x < 0:  # Assuming robot is left of the marker
