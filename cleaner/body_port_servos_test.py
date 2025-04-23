ad1c8efd4f7737e21ee3661dd10e04f5f7a1c9a3
Jackson Kueny <jacksonkueny4@gmail.com>
2025-04-23 12:52:53 -0600

    Update body_port_servos_test.py

--- cleaner/body_port_servos_test.py
+++ cleaner/body_port_servos_test.py
@@ -9,7 +9,7 @@ WAIST_ROTATION = 2
 HEAD_UP_DOWN_PORT = 3
 HEAD_LEFT_RIGHT_PORT = 4
 
-TEST_PORT = 4
+TEST_PORT = 5
 
 class RobotControl:
     _instance = None
@@ -22,15 +22,37 @@ class RobotControl:
 
     def __init__(self):
         self.m = Controller()
-        
+
     def body_reset(self):
+        self.m.setTarget(2, 6000)
+        self.m.setTarget(3, 6000)
+        self.m.setTarget(4, 6000)
+        self.m.setTarget(5, 6000)
+        self.m.setTarget(6, 6000)
+        self.m.setTarget(7, 6000)
+        self.m.setTarget(8, 6000)
+        self.m.setTarget(9, 6000)
+        self.m.setTarget(10, 6000)
+        self.m.setTarget(11, 6000)
+        self.m.setTarget(12, 6000)
+        self.m.setTarget(13, 6000)
+        self.m.setTarget(14, 6000)
+        self.m.setTarget(15, 6000)
+        self.m.setTarget(16, 6000)
+        self.m.setTarget(17, 6000)
+        self.m.setTarget(18, 6000)
+        self.m.setTarget(19, 6000)
+        self.m.setTarget(20, 6000)
+
+    def servo_test(self):
         self.m.setTarget(TEST_PORT, 7500)
         time.sleep(1)
-        self.m.setTarget(TEST_PORT, 600)
+        self.m.setTarget(TEST_PORT, 6000)
 
 robot = RobotControl()
 
 def main():
     robot.body_reset()
+    #robot.servo_test()
     
 main()
